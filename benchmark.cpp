5110ee0d10f3743a5caa049e2c7ea7e3b25e4307
fanliwen <fanliwen@zhihu.com>
2019-01-25 16:09:34 +0800

    compare results from cuBERT and tfBERT

--- benchmark.cpp
+++ benchmark.cpp
@@ -2,82 +2,24 @@
 #include <chrono>
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
-#include "tensorflow/c/c_api.h"
 #include "cuBERT.h"
 
-TF_Output t_inputs[3];
-TF_Output t_output;
-
-TF_Session* open_tf(const char* export_dir) {
-    const char* tag = "serve";
-    TF_Status* status = TF_NewStatus();
-
-    /*
->>> config = tf.ConfigProto(allow_soft_placement=True)
->>> config.gpu_options.allow_growth = True
->>> config.gpu_options.visible_device_list = '1'
-     */
-    TF_SessionOptions* opts = TF_NewSessionOptions();
-    unsigned char config[9] = {0x32, 0x05, 0x20, 0x01, 0x2a, 0x01, 0x31, 0x38, 0x01};
-    TF_SetConfig(opts, config, 9, status);
-
-    TF_Graph* graph = TF_NewGraph();
-    TF_Session* session = TF_LoadSessionFromSavedModel(opts, nullptr, export_dir, &tag, 1, graph, nullptr, status);
-
-    t_inputs[0] = {TF_GraphOperationByName(graph, "input_ids"), 0};
-    t_inputs[1] = {TF_GraphOperationByName(graph, "input_mask"), 0};
-    t_inputs[2] = {TF_GraphOperationByName(graph, "segment_ids"), 0};
-    t_output = {TF_GraphOperationByName(graph, "loss/output"), 0};
-
-    return session;
-}
-
-void compute_tf(TF_Session* session,
-                TF_Tensor* input_ids, TF_Tensor* input_mask, TF_Tensor* segment_ids, TF_Tensor** output) {
-    TF_Tensor* input_values[3] = {input_ids, input_mask, segment_ids};
-
-    TF_Status* status = TF_NewStatus();
-    TF_SessionRun(session, nullptr,
-                  t_inputs, input_values, 3,
-                  &t_output, output, 1,
-                  nullptr, 0,
-                  nullptr, status);
-    TF_DeleteStatus(status);
-}
-
-void close_tf(TF_Session* session) {
-    TF_DeleteSession(session, nullptr);
-}
-
-void random_input(int* input_ids, char* input_mask, char* segment_ids, size_t length,
-                  TF_Tensor* tf_input_ids, TF_Tensor* tf_input_mask, TF_Tensor* tf_segment_ids) {
-    std::random_device r;
-    std::default_random_engine e(r());
-
+void random_input(const std::default_random_engine& e,
+                  int* input_ids, char* input_mask, char* segment_ids, size_t length) {
     std::uniform_int_distribution<int> id_dist(0, 21120);
     std::uniform_int_distribution<int> zo_dist(0, 1);
     for (int i = 0; i < length; ++i) {
         input_ids[i] = id_dist(e);
         input_mask[i] = zo_dist(e);
         segment_ids[i] = zo_dist(e);
-
-        ((int64_t*) TF_TensorData(tf_input_ids))[i] = input_ids[i];
-        ((int64_t*) TF_TensorData(tf_input_mask))[i] = input_mask[i];
-        ((int64_t*) TF_TensorData(tf_segment_ids))[i] = segment_ids[i];
-    }
-}
-
-void compare_output(float* logits, size_t length, TF_Tensor* output) {
-    for (int i = 0; i < length; ++i) {
-        float tf = ((float*) TF_TensorData(output))[i];
-        if (fabsf(tf - logits[i]) >= 1e-5) {
-            std::cout << "err: i=" << i << ", cuBERT=" << logits[i] << ", TF=" << tf << std::endl;
-        }
     }
 }
 
 int main() {
+    std::default_random_engine e(0);
+
     int max_batch_size = 512;
     int batch_size = 400;
     int seq_length = 32;
@@ -88,21 +30,12 @@ int main() {
     char segment_ids[batch_size * seq_length];
     float logits[batch_size];
 
-    // tensorflow
-    int64_t dims[2] = {batch_size, seq_length};
-    TF_Tensor* tf_input_ids = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
-    TF_Tensor* tf_input_mask = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
-    TF_Tensor* tf_segment_ids = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
-    std::cout << TF_Version() << std::endl;
-
     void* model = cuBERT_open("bert_frozen_seq32.pb", max_batch_size, seq_length, 12, 12);
-    TF_Session* tf_model = open_tf("bert");
+    std::ofstream result("cuBERT.txt");
 
     std::cout << "=== warm_up ===" << std::endl;
     for (int i = 0; i < 10; ++i) {
-        TF_Tensor* output;
-        random_input(input_ids, input_mask, segment_ids, batch_size * seq_length,
-                     tf_input_ids, tf_input_mask, tf_segment_ids);
+        random_input(e, input_ids, input_mask, segment_ids, batch_size * seq_length);
 
         auto start = std::chrono::high_resolution_clock::now();
         cuBERT_compute(model, batch_size, input_ids, input_mask, segment_ids, logits);
@@ -110,21 +43,14 @@ int main() {
         long long milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
         std::cout << "cuBERT: " << milli << "ms" << std::endl;
 
-        start = std::chrono::high_resolution_clock::now();
-        compute_tf(tf_model, tf_input_ids, tf_input_mask, tf_segment_ids, &output);
-        finish = std::chrono::high_resolution_clock::now();
-        milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
-        std::cout << "TF: " << milli << "ms" << std::endl;
-
-        compare_output(logits, batch_size, output);
-        TF_DeleteTensor(output);
+        for (int j = 0; j < batch_size; ++j) {
+            result << logits[j] << std::endl;
+        }
     }
 
     std::cout << "=== benchmark ===" << std::endl;
     for (int i = 0; i < 10; ++i) {
-        TF_Tensor* output;
-        random_input(input_ids, input_mask, segment_ids, batch_size * seq_length,
-                     tf_input_ids, tf_input_mask, tf_segment_ids);
+        random_input(e, input_ids, input_mask, segment_ids, batch_size * seq_length);
 
         auto start = std::chrono::high_resolution_clock::now();
         cuBERT_compute(model, batch_size, input_ids, input_mask, segment_ids, logits);
@@ -132,20 +58,11 @@ int main() {
         long long milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
         std::cout << "cuBERT: " << milli << "ms" << std::endl;
 
-        start = std::chrono::high_resolution_clock::now();
-        compute_tf(tf_model, tf_input_ids, tf_input_mask, tf_segment_ids, &output);
-        finish = std::chrono::high_resolution_clock::now();
-        milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
-        std::cout << "TF: " << milli << "ms" << std::endl;
-
-        compare_output(logits, batch_size, output);
-        TF_DeleteTensor(output);
+        for (int j = 0; j < batch_size; ++j) {
+            result << logits[j] << std::endl;
+        }
     }
 
-    TF_DeleteTensor(tf_segment_ids);
-    TF_DeleteTensor(tf_input_mask);
-    TF_DeleteTensor(tf_input_ids);
-    close_tf(tf_model);
-
+    result.close();
     cuBERT_close(model);
 }
---
+++ benchmark_tf.cpp
@@ -0,0 +1,113 @@
+#include <random>
+#include <chrono>
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+#include "tensorflow/c/c_api.h"
+
+TF_Output t_inputs[3];
+TF_Output t_output;
+
+TF_Session* tf_open(const char *export_dir) {
+    const char* tag = "serve";
+    TF_Status* status = TF_NewStatus();
+    TF_SessionOptions* opts = TF_NewSessionOptions();
+
+    TF_Graph* graph = TF_NewGraph();
+    TF_Session* session = TF_LoadSessionFromSavedModel(opts, nullptr, export_dir, &tag, 1, graph, nullptr, status);
+
+    t_inputs[0] = {TF_GraphOperationByName(graph, "input_ids"), 0};
+    t_inputs[1] = {TF_GraphOperationByName(graph, "input_mask"), 0};
+    t_inputs[2] = {TF_GraphOperationByName(graph, "segment_ids"), 0};
+    t_output = {TF_GraphOperationByName(graph, "loss/output"), 0};
+
+    return session;
+}
+
+void tf_compute(TF_Session *session,
+                TF_Tensor *input_ids, TF_Tensor *input_mask, TF_Tensor *segment_ids, TF_Tensor **output) {
+    TF_Tensor* input_values[3] = {input_ids, input_mask, segment_ids};
+
+    TF_Status* status = TF_NewStatus();
+    TF_SessionRun(session, nullptr,
+                  t_inputs, input_values, 3,
+                  &t_output, output, 1,
+                  nullptr, 0,
+                  nullptr, status);
+    TF_DeleteStatus(status);
+}
+
+void tf_close(TF_Session *session) {
+    TF_DeleteSession(session, nullptr);
+}
+
+void random_input(const std::default_random_engine& e,
+                  TF_Tensor* tf_input_ids, TF_Tensor* tf_input_mask, TF_Tensor* tf_segment_ids, size_t length) {
+    std::uniform_int_distribution<int> id_dist(0, 21120);
+    std::uniform_int_distribution<int> zo_dist(0, 1);
+    for (int i = 0; i < length; ++i) {
+        ((int64_t*) TF_TensorData(tf_input_ids))[i] = id_dist(e);
+        ((int64_t*) TF_TensorData(tf_input_mask))[i] = zo_dist(e);
+        ((int64_t*) TF_TensorData(tf_segment_ids))[i] = zo_dist(e);
+    }
+}
+
+int main() {
+    std::default_random_engine e(0);
+
+    int max_batch_size = 512;
+    int batch_size = 400;
+    int seq_length = 32;
+
+    // tensorflow
+    int64_t dims[2] = {batch_size, seq_length};
+    TF_Tensor* tf_input_ids = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
+    TF_Tensor* tf_input_mask = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
+    TF_Tensor* tf_segment_ids = TF_AllocateTensor(TF_INT64, dims, 2, sizeof(int64_t) * dims[0] * dims[1]);
+    std::cout << TF_Version() << std::endl;
+
+    TF_Session* tf_model = tf_open("bert");
+    std::ofstream result("tfBERT.txt");
+
+    std::cout << "=== warm_up ===" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        TF_Tensor* output;
+        random_input(e, tf_input_ids, tf_input_mask, tf_segment_ids, batch_size * seq_length);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        tf_compute(tf_model, tf_input_ids, tf_input_mask, tf_segment_ids, &output);
+        auto finish = std::chrono::high_resolution_clock::now();
+        long long milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+        std::cout << "TF: " << milli << "ms" << std::endl;
+
+        for (int j = 0; j < batch_size; ++j) {
+            result << ((float*) TF_TensorData(output))[j] << std::endl;
+        }
+        TF_DeleteTensor(output);
+    }
+
+    std::cout << "=== benchmark ===" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        TF_Tensor* output;
+        random_input(e, tf_input_ids, tf_input_mask, tf_segment_ids, batch_size * seq_length);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        tf_compute(tf_model, tf_input_ids, tf_input_mask, tf_segment_ids, &output);
+        auto finish = std::chrono::high_resolution_clock::now();
+        long long milli = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+        std::cout << "TF: " << milli << "ms" << std::endl;
+
+        for (int j = 0; j < batch_size; ++j) {
+            result << ((float*) TF_TensorData(output))[j] << std::endl;
+        }
+        TF_DeleteTensor(output);
+    }
+
+    TF_DeleteTensor(tf_segment_ids);
+    TF_DeleteTensor(tf_input_mask);
+    TF_DeleteTensor(tf_input_ids);
+
+    result.close();
+    tf_close(tf_model);
+}
