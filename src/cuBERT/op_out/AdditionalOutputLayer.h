0d48bae12ee6bfc4a67558fe78ee522571efcdec
fanliwen <fanliwen@zhihu.com>
2019-03-11 14:40:30 +0800

    refine doc string

--- src/cuBERT/op_out/AdditionalOutputLayer.h
+++ src/cuBERT/op_out/AdditionalOutputLayer.h
@@ -5,6 +5,16 @@
 #include <cstddef>
 
 namespace cuBERT {
+
+    /**
+     * output_layer = model.get_pooled_output()
+     *
+     * output_weights = tf.get_variable(
+     *   "output_weights", [1, hidden_size],
+     *   initializer=tf.truncated_normal_initializer(stddev=0.02))
+     *
+     * logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+     */
     class AdditionalOutputLayer {
     public:
         explicit AdditionalOutputLayer(void* handle, size_t hidden_size, float *output_weights);
